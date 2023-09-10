c59d5973476dc3e8ade0997aa341bee7f81e8079
sadrbox <sadrbox@mail.ru>
2023-09-10 18:26:38 +0600

    Sun, Sep 10, 2023  6:26:38 PM

---
+++ vladilenminin/react-typescript/src/App.tsx
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import { Navbar } from "./components/Navbar";
+import { TodoForm } from "./components/TodoForm";
+import { TodoList } from "./components/TodoList";
+import { ITodo } from "./interfaces";
+
+const App: React.FC = () => {
+	const [todos, setTodos] = useState<ITodo[]>([]);
+	const addHandler = (title: string) => {
+		const newTodo: ITodo = {
+			title: title,
+			id: Date.now(),
+			completed: false,
+		};
+		// setTodos([newTodo, ...todos]);
+		setTodos((prev) => [newTodo, ...prev]);
+	};
+
+	const toggleHandler = (id: number) => {
+		setTodos((prev) =>
+			prev.map((todo) => {
+				if (todo.id === id) {
+					todo.completed = !todo.completed;
+				}
+				return todo;
+			})
+		);
+	};
+	const removeHandler = (id: number) => {
+		setTodos((prev) => prev.filter((todo) => todo.id !== id));
+	};
+	return (
+		<>
+			<Navbar />
+			<div className="container">
+				<TodoForm onAdd={addHandler} />
+				<TodoList
+					todos={todos}
+					onToggle={toggleHandler}
+					onRemove={removeHandler}
+				/>
+			</div>
+		</>
+	);
+};
+
+export default App;
---
+++ vladilenminin/react-typescript/src/components/Navbar.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export const Navbar: React.FC = () => (
+	<nav>
+		<div className="nav-wrapper purple darken-2 px1">
+			<a href="#" className="brand-logo">
+				React + TypeScript
+			</a>
+			<ul className="right hide-on-med-and-down">
+				<li>
+					<a href="/">Списко дел</a>
+				</li>
+				<li>
+					<a href="/">Информация</a>
+				</li>
+			</ul>
+		</div>
+	</nav>
+);
