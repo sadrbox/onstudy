56c447bd1de8668c3e931fd85134a93160456898
sadrbox <sadrbox@mail.ru>
2023-09-14 22:48:30 +0600

    Thu, Sep 14, 2023 10:48:29 PM

--- vladilenminin/react-crash-course-2022/src/App.tsx
+++ vladilenminin/react-crash-course-2022/src/App.tsx
@@ -2,24 +2,33 @@ import React, { useEffect, useState } from "react";
 import "./App.css";
 import { Product } from "./components/Product";
 // import { products } from "./data/products";
-import axios from "axios";
+// import axios from "axios";
+import axios, { AxiosError } from "axios";
 import { IProduct } from "./models";
 import Loading from "./components/Loading";
 
 function App() {
 	const [products, setProducts] = useState<IProduct[]>([]);
 	const [loading, setLoading] = useState(false);
+	const [error, setError] = useState("");
 
 	async function fetchProducts() {
-		setLoading(true);
-		const response = await axios.get<IProduct[]>(
-			"https://fakestoreapi.com/products?limit=5"
-		);
+		try {
+			setError("");
+			setLoading(true);
+			const response = await axios.get<IProduct[]>(
+				"https://fakestoreapi.com/products?limit=5"
+			);
 
-		setTimeout(() => {
-			setProducts(response.data);
+			setTimeout(() => {
+				setProducts(response.data);
+				setLoading(false);
+			}, 2000);
+		} catch (e) {
+			const error = e as AxiosError;
 			setLoading(false);
-		}, 2000);
+			setError(error.message);
+		}
 	}
 
 	useEffect(() => {
@@ -28,13 +37,14 @@ function App() {
 
 	return (
 		<div className="container mx-auto max-w-2xl pt-5 grid h-screen place-items-center">
-			{products.length > 0 ? (
+			{!loading ? (
 				products.map((product, index) => (
 					<Product key={product.id} product={product} />
 				))
 			) : (
 				<Loading />
 			)}
+			{error && <p className="text-center text-red-500">{error}</p>}
 		</div>
 	);
 }
