5acdc8f8c55e880ab74567dca2f9ed5962857e3a
sadrbox <sadbox@mail.ru>
2023-09-14 14:53:15 +0600

    Thu, Sep 14, 2023  2:53:15 PM

--- vladilenminin/react-crash-course-2022/src/App.tsx
+++ vladilenminin/react-crash-course-2022/src/App.tsx
@@ -1,14 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import { Product } from "./components/Product";
+// import { products } from "./data/products";
+import axios from "axios";
+import { IProduct } from "./models";
+import Loading from "./components/Loading";
 
 function App() {
+	const [products, setProducts] = useState<IProduct[]>([]);
+	const [loading, setLoading] = useState(false);
+
+	async function fetchProducts() {
+		setLoading(true);
+		const response = await axios.get<IProduct[]>(
+			"https://fakestoreapi.com/products?limit=5"
+		);
+
+		setTimeout(() => {
+			setProducts(response.data);
+			setLoading(false);
+		}, 2000);
+	}
+
+	useEffect(() => {
+		fetchProducts();
+	}, []);
+
 	return (
-		<div className="container">
-			<Product />
-			<Product />
-			<Product />
-			<Product />
+		<div className="container mx-auto max-w-2xl pt-5 grid h-screen place-items-center">
+			{products.length > 0 ? (
+				products.map((product, index) => (
+					<Product key={product.id} product={product} />
+				))
+			) : (
+				<Loading />
+			)}
 		</div>
 	);
 }
