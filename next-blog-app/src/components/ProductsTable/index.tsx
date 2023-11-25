1009949e78eedf0379b1f44d598859b214a3915a
sadrbox <sadbox@mail.ru>
2023-11-25 18:33:12 +0600

    25 ноя 2023 г. 18:33:12

--- next-blog-app/src/components/ProductsTable/index.tsx
+++ next-blog-app/src/components/ProductsTable/index.tsx
@@ -1,105 +1,36 @@
-// "use client";
-
-import React, { FC } from "react";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
-
-import { TProducts, TProduct } from "@/src/queries";
+"use client";
+import { TProducts } from "@/src/queries";
+import React from "react";
 import styles from "./ProductsTable.module.scss";
-import Image from "next/image";
-import { Box } from "@mui/material";
 
-const columns = [
-  // { id: "image", name: "", width: "10%" },
-  { id: "id", name: "ID", width: 50 },
-  { id: "title", name: "Title", width: 100 },
-  { id: "price", name: "Price", width: "10%" },
-  // { id: "description", name: "Description", width: "20%" },
-  { id: "category", name: "Category", width: "20%" },
-  { id: "rating", name: "Rating", width: "10%" },
-];
 export default function ProductsTable({ products }: { products: TProducts }) {
   return (
-    <Box sx={{ p: "2px", bgcolor: "#ddd", borderRadius: "2px" }}>
-      <TableContainer
-        sx={{
-          overflow: "scroll",
-          maxHeight: 400,
-          backgroundColor: "#fff",
-        }}
-      >
-        <Table
-          stickyHeader
-          size="small"
-          className={styles.DataTable}
-          aria-label="simple table"
-        >
-          <TableHead>
-            <TableRow>
-              {columns &&
-                columns.map((column) => {
-                  return (
-                    <TableCell sx={{}} key={column.id}>
-                      {column.name}
-                    </TableCell>
-                  );
-                })}
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {products &&
-              products.map((item) => {
-                return (
-                  <TableRow key={item.id}>
-                    {/* {columns.map((column) => {
-                    const tableCellWidth = columns.find(
-                      (fndCol) => fndCol.id === column.id
-                    )?.width;
-                    return (
-                      <TableCell key={column.id} sx={{ width: tableCellWidth }}>
-                        {item[column.id]}
-                      </TableCell>
-                    );
-                  })} */}{" "}
-                    {/* <TableCell
-                    sx={{ maxWidth: 30, maxHeight: 28, overflow: "hidden" }}
-                  >
-                    <Image
-                      // style={{ maxHeight: "50px", width: "auto" }}
-                      src={item.image}
-                      width={28}
-                      height={28}
-                      alt="Picture of the author"
-                    />
-                  </TableCell> */}
-                    <TableCell sx={{ minWidth: 20, maxWidth: 100 }}>
-                      {item.id}
-                    </TableCell>
-                    <TableCell sx={{ minWidth: 100, maxWidth: 200 }}>
-                      {item.title}
-                    </TableCell>
-                    <TableCell sx={{ minWidth: 50, maxWidth: 100 }}>
-                      {item.price}
-                    </TableCell>
-                    <TableCell sx={{ minWidth: 50, maxWidth: 100 }}>
-                      {item.category}
-                    </TableCell>
-                    <TableCell sx={{ minWidth: 50, maxWidth: 100 }}>
-                      {item.rating?.rate}
-                    </TableCell>
-                  </TableRow>
-                );
-              })}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    </Box>
+    <>
+      <table className={styles.DataTable}>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Title</th>
+            <th>Price</th>
+            <th>Rating</th>
+            <th>Category</th>
+          </tr>
+        </thead>
+        <tbody>
+          {products &&
+            products.map((item) => {
+              return (
+                <tr key={item.id}>
+                  <td>{item.id}</td>
+                  <td>{item.title}</td>
+                  <td align="center">{item.price}</td>
+                  <td align="center">{item.rating?.rate}</td>
+                  <td>{item.category}</td>
+                </tr>
+              );
+            })}
+        </tbody>
+      </table>
+    </>
   );
 }
-
-// export default ProductsTable;
