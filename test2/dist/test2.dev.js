55c2789b02d9b97ffdbf2fa4e257602af9281cdb
sadrbox <sadbox@mail.ru>
2024-01-17 01:07:14 +0600

    17 янв 2024 г.  1:07:14

--- test2/dist/test2.dev.js
+++ test2/dist/test2.dev.js
@@ -8,17 +8,31 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "d
 
 _dotenv["default"].config();
 
-// Создаем объект COM-соединения с ConnectWorkingProcess
-var connector = new _winax["default"].Object("V83.COMConnector");
-var connectWorkingProcess = connector.ConnectAgent("tcp://server:1540");
-var clusters = connectWorkingProcess.GetClusters();
-connectWorkingProcess.Authenticate(clusters[0], "admin", "Qwe123"); // connectWorkingProcess.AuthenticateAgent("admin", "Qwe123");
-// const admins = connectWorkingProcess.GetAgentAdmins();
+function getData() {
+  try {
+    var connector = new _winax["default"].Object("V83.COMConnector");
+    var connectWorkingProcess = connector.ConnectAgent("tcp://server:1540");
+    var clusters = connectWorkingProcess.GetClusters();
+    connectWorkingProcess.Authenticate(clusters[0], "admin", "Qwe123"); // connectWorkingProcess.AuthenticateAgent("admin", "Qwe123");
+    // const admins = connectWorkingProcess.GetAgentAdmins();
 
-var infobases = connectWorkingProcess.GetInfoBases(clusters[0]);
-infobases.map(function (base) {
-  console.log(base.Descr);
-}); // const clusterServices = connectWorkingProcess.GetClusterManagers(clusters[0]);
+    var _infobases = connectWorkingProcess.GetInfoBases(clusters[0]); // return infobases;
+
+
+    var arr = [];
+
+    _infobases.map(function (base) {
+      arr.push(base.name);
+    });
+
+    return arr;
+  } catch (e) {
+    return e;
+  }
+}
+
+var infobases = getData();
+console.log(JSON.stringify(infobases)); // const clusterServices = connectWorkingProcess.GetClusterManagers(clusters[0]);
 // console.log(cluster);
 // const auth = connectWorkingProcess.Authenticate(cluster, "admin", "Qwe123");
 // while (clusters) {
