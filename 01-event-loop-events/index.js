2cea2ed2b10ea72b53c2e05d90fde7f9e7c4d734
sadrbox <sadrbox@mail.ru>
2024-04-02 23:07:22 +0500

    2 апр 2024 г. 23:07:22

--- 01-event-loop-events/index.js
+++ 01-event-loop-events/index.js
@@ -1,5 +1,24 @@
+const fs = require("fs");
+let isRunning = true;
+
 console.log("Program start");
 
-setTimeout(() => console.log("Timeout 1"), 100);
+setTimeout(() => (isRunning = false), 10);
 
+process.nextTick(() => console.log("Next tick"));
+
+function setImmediatePromise() {
+	return new Promise((resolve, reject) => {
+		setImmediate(() => resolve());
+	});
+}
+
+async function whileLoop() {
+	while (isRunning) {
+		console.log("While loop is running...");
+		await setImmediatePromise();
+	}
+}
+
+whileLoop().then(() => console.log("While loop ended"));
 console.log("Program end");
