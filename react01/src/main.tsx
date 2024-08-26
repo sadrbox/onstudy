cf3cc70637a2e2cd845d7d373da85ef31f524aff
SADR <sadrbox@mail.ru>
2024-08-26 14:55:43 +0500

    26 авг 2024 г. 14:55:43

--- react01/src/main.tsx
+++ react01/src/main.tsx
@@ -1,18 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 import Post from "./objects/Post";
 // import DataGrid from "./ui/DataGrid/index.tsx";
 import "./index.css";
-import { Provider } from "jotai";
+// import { Provider } from "jotai";
 import Products from "./objects/Products";
 import Users from "./objects/Users";
 import TabBar from "./ui/TabBar";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Post />,
+  },
+  {
+    path: "/products",
+    element: <Products />,
+  },
+  {
+    path: "/users/lst",
+    element: <Users />,
+  },
+  { path: "/users/elm", element: <Users /> },
+]);
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  // <React.StrictMode>
-  <Provider>
+  <React.StrictMode>
     <TabBar />
-    <Users />
-  </Provider>,
-  // </React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
 );
